5543192d14f074b1b7abe6ad9e77465f0cde6f30
Daniel Boffey <dboffey70@googlemail.com>
2019-12-12 20:02:34 +0000

    main pages done, only work to complete

--- docs/js/app.js
+++ docs/js/app.js
@@ -1,77 +1,3 @@
-// const TypeWriter = function(txtElement, words, wait = 3000) {
-//   this.txtElement = txtElement;
-//   this.words = words;
-//   this.txt = '';
-//   this.wordIndex = 0;
-//   this.wait = parseInt(wait, 10);
-//   this.type();
-//   this.isDeleting = false;
-// }
-
-// // Type Method
-// TypeWriter.prototype.type = function() {
-//   // Current index of word array
-//   const current = this.wordIndex % this.words.length;
-//   // Get full text of current word
-//   const fullTxt = this.words[current];
-//   // Check if deleting
-//   if(this.isDeleting) {
-//     // Remove char
-//     this.txt = fullTxt.substring(0, this.txt.length - 1);
-//   } else {
-//     // Add char
-//     this.txt = fullTxt.substring(0, this.txt.length + 1);
-//   }
-//
-//   // Insert txt into element
-//   this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
-//
-//   // Initial Type speed
-//   let typeSpeed = 300;
-//
-//   if(this.isDeleting) {
-//     typeSpeed /= 2;
-//   }
-//
-//   // If word is complete
-//   if(!this.isDeleting && this.txt === fullTxt) {
-//     // Make pause at end of word written
-//     typeSpeed = this.wait;
-//     // Set delete to true
-//     this.isDeleting = true;
-//   } else if(this.isDeleting && this.txt === ''){
-//     this.isDeleting = false;
-//     // Move to next word
-//     this.wordIndex++;
-//     // Pause before start typing
-//     typeSpeed = 1500;
-//   }
-//
-//   setTimeout(() => this.type(), 500)
-// }
-
-// Smooth scrolling
-$(".cf a").on("click", function(event) {
-  if (this.hash !== "") {
-    event.preventDefault();
-
-    const hash = this.hash;
-
-    $("html, body").animate(
-      {
-        scrollTop: $(hash).offset().top
-      },
-      800,
-      function() {
-        window.location.hash = hash;
-      }
-    );
-  }
-});
-
-// Get the current year for the copyright
-$("#year").text(new Date().getFullYear());
-
 // ES6 class
 class TypeWriter {
   constructor(txtElement, words, wait = 3000) {
@@ -138,16 +64,6 @@ function init() {
   new TypeWriter(txtElement, words, wait);
 }
 
-// function openSlideMenu(){
-//   document.getElementById('side-menu').style.width = '250px';
-//   // document.getElementById('main').style.marginLeft = '250px';
-//   }
-//
-//   function closeSideMenu(){
-//     document.getElementById('side-menu').style.width = '0';
-//     // document.getElementById('main').style.marginLeft = '0';
-//     }
-
 /* Set the width of the side navigation to 250px */
 function openNav() {
   document.getElementById("mySidenav").style.width = "250px";
@@ -171,4 +87,38 @@ function animateCSS(element, animationName, callback) {
   }
 
   node.addEventListener("animationend", handleAnimationEnd);
+}
+
+// carousel
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
 }
